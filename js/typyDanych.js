0f42d2aa468591554edea24ae2db37b9f4e1441f
Michał Kozłowski <kozlowskimichal88@gmail.com>
2017-06-10 10:18:31 +0200

    stworzenie plikow i pierwsze dzialanie

---
+++ js/typyDanych.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+/*
+
+Typ liczbowy
+
+*/
+
+
+var wyplata = 5000;
+var premia = 2500;
+var calkowitaWyplata;
+
+calkowitaWyplata = wyplata + premia;
+
+console.log(calkowitaWyplata);
